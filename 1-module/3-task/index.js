74a0133fb6821f1cbd98fe65bc33adf4111e26a6
Ilya Kantor <glimah481@gmail.com>
2020-06-05 18:59:46 +0300

    module1task1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @param {string} str
  * @returns {string}
  */
-function ucFirst(str) {
-}
+
+
+  function ucFirst(str){
+    if (!str) return str;
+    return  str[0].toUpperCase() + str.slice(1);
+
+  }
+
+
+
