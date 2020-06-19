27dd0c9c55a5a05af39f19e6cc4b4cb505fb41c0
Ilya Kantor <glimah481@gmail.com>
2020-06-19 22:49:57 +0300

    module2task1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,24 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
+const salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
 
+function sumSalary(sal) {
+  let sum = 0;
+  for (const key in sal) {
+    if (typeof sal[key] === "number") {
+      sum = sum + sal[key];
+    }
+  }
+  return sum;
 }
+console.log(sumSalary(salaries));
+
+
